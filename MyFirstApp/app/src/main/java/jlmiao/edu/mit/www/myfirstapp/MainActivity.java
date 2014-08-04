6b4aea87b9855ca6a0b5f7bf130bfbaf1538dd7c
Jilang Miao 缪佶朗 @ windows <m.j.long0619@gmail.com>
2014-08-04 00:10:29 -0400

    respon functions

--- MyFirstApp/app/src/main/java/jlmiao/edu/mit/www/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/jlmiao/edu/mit/www/myfirstapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.EditText;
+import android.view.MenuInflater;
 
 public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -23,14 +24,22 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // If your minSdkVersion is 11 or higher, instead use:
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.main, menu);
+        //return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        //main --> src/main/res/memu/main.xml
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -38,10 +47,16 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
